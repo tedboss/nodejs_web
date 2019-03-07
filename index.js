1c2669c51e3a4613e734acc5d9431bdc00c536e4
Tâm Tâm <tam.codientu16@gmail.com>
2019-03-07 13:59:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
+    for (var i = 0; i < 100; i++)
+    {
+        response.write("Hello World! ahihi ahihi " + i + "\n");
+    }
     response.end("Hello World! ahihi ahihi");
 
 });
